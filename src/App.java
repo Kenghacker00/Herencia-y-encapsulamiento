fa947e353be3a377220db18e790dbb5cac5bcb09
Kenghacker00 <hgonzalezfonseca2@gmail.com>
2024-05-24 18:13:40 -0600

    Sin el independiente

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Crear instancias de cada tipo de vehículo
+        Auto auto = new Auto("Toyota", "Corolla", 2020, 15000, 5, "Gasolina");
+
+        Camion camion = new Camion("Volvo", "FH", 2019, 75000, 20000, 4);
+
+        // Mostrar la informacion de cada vehiculo
+        System.out.println("Informacion del Auto:");
+        auto.mostrarInfo();
+
+        System.out.println("\nInformacion del Camion:");
+        camion.mostrarInfo();
+    }
+
+}
